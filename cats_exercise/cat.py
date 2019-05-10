450c95eef15bb72d4993702196ddb6850ec05f5f
Jasmin Saromo <jasminsaromo@gmail.com>
2019-05-10 15:47:27 -0400

    added eats_at instance

--- cats_exercise/cat.py
+++ cats_exercise/cat.py
@@ -7,7 +7,13 @@ class Cat:
         self.time = meal_time
 
     def __str__(self):
-        return f"Name: {self.name} Preferred food: {self.food} Usual meal time: {self.time}"
+        return f"Name: {self.name} Preferred food: {self.food} Usual meal time: {self.eats_at()}"
+
+    def eats_at(self):
+        if self.time >= 13:
+            return f"{self.time - 12} PM"
+        else:
+            return f"{self.time} AM"
 
 muca = Cat("Muca", "dry food", 8)
 chava = Cat("Chava", "whatever you're eating", 14)

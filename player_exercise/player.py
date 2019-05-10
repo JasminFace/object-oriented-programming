b3eaee1b5ba20f6e49afcd9871b68c49a82c6f4f
Jasmin Saromo <jasminsaromo@gmail.com>
2019-05-10 16:02:54 -0400

    created player class

---
+++ player_exercise/player.py
@@ -0,0 +1,6 @@
+class Player:
+    """ A class that represents a player """
+    def __init__(self, gold_coins, health_points, lives):
+        self.gold_coins = 0
+        self.health_points = 10
+        self.lives = 5

844ed5e3c4bc2633b16f171dcc6e6cfaff5bbb75
Jasmin Saromo <jasminsaromo@gmail.com>
2019-05-10 16:21:42 -0400

    added __str__ instance method

--- player_exercise/player.py
+++ player_exercise/player.py
@@ -1,6 +1,12 @@
 class Player:
     """ A class that represents a player """
-    def __init__(self, gold_coins, health_points, lives):
+    def __init__(self):
         self.gold_coins = 0
         self.health_points = 10
         self.lives = 5
+
+    def __str__(self):
+        return f"Lives: {self.lives}  Health: {self.health_points}  Gold Coins: {self.gold_coins}"
+
+player1 = Player()
+print(player1)

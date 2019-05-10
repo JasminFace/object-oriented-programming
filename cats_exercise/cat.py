c8c402215364b42aeb04640957641e85926429c3
Jasmin Saromo <jasminsaromo@gmail.com>
2019-05-10 15:39:30 -0400

    created class for cat exercise

---
+++ cats_exercise/cat.py
@@ -0,0 +1,7 @@
+class Cat:
+    """ A class representing cats """
+
+    def __init__(self, name, preferred_food, meal_time):
+        self.name = name
+        self.food = preferred_food
+        self.time = meal_time
